c505fb23414a41be270ad09456db3de909ee5705
KanameS <nk23a4@gmail.com>
2021-11-22 14:37:50 +0700

    Update Autohouse.py

--- Autohouse.py
+++ Autohouse.py
@@ -3,7 +3,7 @@
 from pyautogui import *
 import time
 
-#
+# just leave it 0.25 for now
 config_delaytime = 0.25
 
 def longclick(button="left"):
@@ -14,16 +14,20 @@ def longclick(button="left"):
 
 while(True):
     x, y = position()
+    # Target middle of screen
     moveTo(987, 547)
     click();
     longclick(button="right")
     time.sleep(config_delaytime)
+    # Buy button
     moveTo(870, 637)
     longclick(button="left")
     time.sleep(config_delaytime)
+    # Free Comapny Button
     moveTo(825, 601)
     longclick(button="left")
     time.sleep(config_delaytime)
+    # Yes/Confirm Button
     moveTo(899, 589)
     longclick(button="left")
     time.sleep(config_delaytime)
